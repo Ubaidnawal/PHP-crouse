13d050e86ec0328187600b48287587d384edcf53
Ubaidnawal <ubaidnawal857@gmail.com>
2026-01-24 13:01:19 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+$num = 5;
+
+if ($num > 0) {
+    echo "positive " . $num;
+} else {
+    echo "not positive " . $num;
+}
+
+   $name = "Ubaid";
+   $age = 20;
+   $student = true;
+
+   echo $name;
+   echo $age;
+   echo $student;
+
+
+
+?>
